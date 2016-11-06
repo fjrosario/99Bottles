977f58ba06442d7514a88e2e8848ac22164875b5
Frank Rosario <FRosario@gmail.com>
2016-11-06 05:54:02 -0500

    Fixed folder naming to work on Mac

---
+++ FRosario.99Bottles.Application/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FRosario.Bottles.App
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("How many bottles of beer?");
+
+			string input = Console.ReadLine();
+
+			int numBottles = 0;
+
+			bool parseSuccess = int.TryParse(input, out numBottles);
+
+			if (parseSuccess == false || numBottles < 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(numBottles), "Number of bottles must be a valid integer that is 0 or greater");
+			}
+			else
+			{
+				Console.WriteLine($"{nameof(numBottles)} = {numBottles}");
+			}
+
+
+			Console.ReadKey();
+		}
+	}
+}
